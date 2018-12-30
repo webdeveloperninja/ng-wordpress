e0a17e4b5159f036f79033c16a12cb2192f09a3e
Your Name <rsmith5901@gmail.com>
2018-12-30 18:15:21 -0500

    adding types

--- src/app/services/authors.service.ts
+++ src/app/services/authors.service.ts
@@ -4,6 +4,7 @@ import { forkJoin, Observable } from 'rxjs';
 
 import { Author } from '../contracts/page';
 import { WordpressService } from './wordpress.service';
+import { PostAuthor } from '../contracts/author';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,12 @@ import { WordpressService } from './wordpress.service';
 export class AuthorsService {
   constructor(private readonly _httpClient: HttpClient, private readonly _wordpressService: WordpressService) {}
 
-  get(authorLinks: Author[]) {
+  get(authorLinks: Author[]): Observable<PostAuthor[]> {
     return this.getAuthors(authorLinks);
   }
 
-  private getAuthors(authorsLinks: Author[]): Observable<any[]> {
-    const requests = authorsLinks.map(author => this._httpClient.get(author.href));
+  private getAuthors(authorsLinks: Author[]): Observable<PostAuthor[]> {
+    const requests = authorsLinks.map(author => this._httpClient.get<PostAuthor>(author.href));
 
     return forkJoin(requests);
   }
