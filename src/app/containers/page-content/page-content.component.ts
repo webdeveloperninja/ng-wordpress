2fc6e4e90ca33f96f9eb3df471f32bb43f698db0
Your Name <rsmith5901@gmail.com>
2018-12-31 11:45:17 -0500

    refactoring authors$ logic into private method

--- src/app/containers/page-content/page-content.component.ts
+++ src/app/containers/page-content/page-content.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { filter, map, mergeMap, withLatestFrom } from 'rxjs/operators';
 import { Page } from 'src/app/contracts/page';
 import { AuthorsService } from 'src/app/services/authors.service';
 import { PagesService } from 'src/app/services/pages.service';
+import { Author } from 'src/app/contracts/author';
 
 @Component({
   selector: 'wp-page-content',
@@ -16,27 +17,20 @@ export class PageContentComponent implements OnInit {
   activePage$ = this.currentSlug$.pipe(
     filter(currentSlug => !!currentSlug),
     withLatestFrom(this.route.params),
-    mergeMap(([currentSlug, params]) =>
+    mergeMap(([slug, params]: [string, Params]) =>
       this._pagesService.get(params.wordpressUrl).pipe(
         map(pages => {
-          return this.getActivePage(pages, currentSlug);
+          return this.getActivePage(pages, slug);
         })
       )
     )
   );
 
-  authors$ = this.activePage$.pipe(
-    mergeMap(page => {
-      const authorLinks = page._links.author;
-
-      return this._authorsService.get(authorLinks);
-    })
-  );
+  authors$ = this.activePage$.pipe(mergeMap(this.getAuthors.bind(this)));
 
   constructor(
     private readonly _pagesService: PagesService,
     private readonly route: ActivatedRoute,
-    private readonly _router: Router,
     private readonly _authorsService: AuthorsService
   ) {}
 
@@ -46,6 +40,12 @@ export class PageContentComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.setRouteSlug(params.slug);
     });
+  }
+
+  private getAuthors(page: Page): Observable<Author[]> {
+    const authorLinks = page._links.author;
+
+    return this._authorsService.get(authorLinks);
   }
 
   private getActivePage(pages: Page[], currentSlug: string): Page {
