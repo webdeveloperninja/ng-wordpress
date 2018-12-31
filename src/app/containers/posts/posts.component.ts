fb350876798096d2f66c00825927d1ac1b657c63
Your Name <rsmith5901@gmail.com>
2018-12-31 12:29:29 -0500

    refactor posts

--- src/app/containers/posts/posts.component.ts
+++ src/app/containers/posts/posts.component.ts
@@ -2,21 +2,21 @@ import { Component } from '@angular/core';
 import { PostsService } from 'src/app/services/posts.service';
 import { tap, mergeMap } from 'rxjs/operators';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/contracts/post';
 
 @Component({
   selector: 'wp-posts',
   templateUrl: './posts.component.html'
 })
 export class PostsComponent {
-  wordpressUrl: string;
-
-  posts$ = this.route.params.pipe(
-    mergeMap((params: Params) => {
-      this.wordpressUrl = params.wordpressUrl;
-
-      return this._postsService.get(this.wordpressUrl);
-    })
-  );
+  posts$ = this.route.params.pipe(mergeMap(this.getPosts.bind(this)));
 
   constructor(private readonly _postsService: PostsService, private readonly route: ActivatedRoute) {}
+
+  private getPosts(params: Params): Observable<Post[]> {
+    const wordpressUrl = params.wordpressUrl;
+
+    return this._postsService.get(wordpressUrl);
+  }
 }
